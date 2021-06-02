c270be0067ef76a758ddc0490d3ae92028f287d9
J_Nam <NamltGCD18728@fpt.edu.vn>
2021-06-02 08:21:07 +0700

    Add AssignedCoursesController, implement CourseTrainer and CourseTrainee

---
+++ HRManagement/Controllers/AssignedCoursesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HRManagement.Models;
+using HRManagement.ViewModels;
+
+namespace HRManagement.Controllers
+{
+    public class AssignedCoursesController : Controller
+    {
+        private ApplicationDbContext _context;
+        public AssignedCoursesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: AssignedCourses
+        public ActionResult Index()
+        {           
+            return View();
+        }
+        public ActionResult CourseTrainer()
+        {
+            var trainerIndb = _context.Users.OfType<Trainer>().Include(t => t.Course).ToList();
+            return View(trainerIndb);
+        }
+        public ActionResult CourseTrainee()
+        {
+            var traineeIndb = _context.Users.OfType<Trainee>().Include(t => t.Course).ToList();
+            return View(traineeIndb);
+        }
+    }
+}
