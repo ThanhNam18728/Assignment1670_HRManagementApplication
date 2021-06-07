91e4b661561301f2389187c7dae5f29b8be23856
J_Nam <NamltGCD18728@fpt.edu.vn>
2021-06-07 09:01:37 +0700

    Add AssignCourseController and implement Index page

---
+++ HRManagement/Controllers/AssignCourseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HRManagement.Models;
+
+namespace HRManagement.Controllers
+{
+    public class AssignCourseController : Controller
+    {
+        private ApplicationDbContext _context;
+        public AssignCourseController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: AssignCourse
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var coures = _context.Courses.ToList();
+            return View(coures);
+        }
+    }
+}
