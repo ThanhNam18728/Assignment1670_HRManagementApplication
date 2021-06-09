59041d59c6f85471e5853ced441f2460da581022
J_Nam <NamltGCD18728@fpt.edu.vn>
2021-06-09 08:28:27 +0700

    Implement AssignCourse Action

--- HRManagement/Controllers/AssignCourseController.cs
+++ HRManagement/Controllers/AssignCourseController.cs
@@ -4,15 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using HRManagement.Models;
+using HRManagement.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace HRManagement.Controllers
 {
     public class AssignCourseController : Controller
     {
+        private UserManager<ApplicationUser> _userManager;
         private ApplicationDbContext _context;
         public AssignCourseController()
         {
             _context = new ApplicationDbContext();
+            _userManager = new UserManager<ApplicationUser>(
+        new UserStore<ApplicationUser>(new ApplicationDbContext()));
         }
         // GET: AssignCourse
         [HttpGet]
@@ -33,6 +39,52 @@ namespace HRManagement.Controllers
             ViewBag.CourseId = id;
 
             return View(users);
+        }
+
+        [HttpGet]
+        public ActionResult AssignCourse(int id)
+        {
+            var users = _context.Users.ToList();
+
+            var usersInCourse = _context.CoursesUsers
+              .Where(t => t.CourseId == id)
+              .Select(t => t.User)
+              .ToList();
+
+            var VModel = new CourseUsersViewModel();
+
+            if (usersInCourse == null)
+            {
+                VModel.CourseId = id;
+                VModel.Users = users;
+
+
+                return View(VModel);
+            }
+
+            var viewModel = new CourseUsersViewModel
+            {
+                CourseId = id,
+                Users = users
+            };
+
+            return View(viewModel);
+        }
+       
+
+        [HttpPost]
+        public ActionResult AssignCourse(CoursesUsers model)
+        {
+            var courseUser = new CoursesUsers
+            {
+                CourseId = model.CourseId,
+                UserId = model.UserId
+            };
+
+            _context.CoursesUsers.Add(courseUser);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ HRManagement/ViewModels/CourseUsersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HRManagement.Models;
+
+namespace HRManagement.ViewModels
+{
+    public class CourseUsersViewModel 
+    {
+        public int CourseId { get; set; }
+        public string UserId { get; set; }
+        public IEnumerable<ApplicationUser> Users { get; set; }
+
+    }
+}
